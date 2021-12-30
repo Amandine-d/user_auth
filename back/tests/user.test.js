6e32c1376215115c36177a8577c16824cec3090c
Amandine <amandine.druart@gmail.com>
2021-12-30 17:21:56 +0100

    Add POST /register route and tests

---
+++ back/tests/user.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../src/server");
+const User = mongoose.model("User");
+
+const user = {
+  email: "test@example.com",
+  password: "p@ssw0rd!",
+};
+
+describe("app", () => {
+
+  it("POST /register", async () => {
+    expect.assertions(2);
+    
+    User.deleteMany({ "email": user.email }).exec();
+
+    const countBefore = await User.countDocuments().exec();
+    const res = await request(app).post("/register").send(user).expect(200);
+    expect(res.body.email).toEqual(user.email);
+    const countAfter = await User.countDocuments().exec();
+    expect(countAfter).toEqual(countBefore + 1);
+
+    User.deleteMany({ "email": user.email }).exec();
+  });
+});
