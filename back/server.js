6dfb59f841ab6b4db38df89b94f41751fa7d91ec
Amandine <amandine.druart@gmail.com>
2021-12-30 17:53:52 +0100

    Move server.js from src to back folder

---
+++ back/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+require("./src/models/user");
+const router = require("./src/routes/users");
+
+
+let config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+// mongoose.connect(`mongodb://${config.user}:${config.password}@${config.host}:${config.port}/${config.database}`).then(() =>
+mongoose.connect("mongodb://localhost/user").then(() => {
+  console.log("connected to mongoDB")
+})
+  .catch((e) => {
+    console.log("Error connecting to mongoDB");
+    console.log(e);
+  });
+const app = express();
+app.use(express.json());
+app.use("/", router);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+})
+
+module.exports = app;
