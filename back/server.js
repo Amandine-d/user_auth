38839b83ce5c61d285a9c9d6b639b22d49c89181
Amandine <amandine.druart@gmail.com>
2021-12-30 11:26:11 +0100

    Set up React for the front part of the app, nodejs/Express for the back. Installation of the useful dependencies. Creation of a server on port 8000

---
+++ back/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+})
+
+module.exports =app;
